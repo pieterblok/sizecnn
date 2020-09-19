541ff893b0288a9c724575cd83fe73c6236b7d5a
Blok, Pieter <pieter.blok@wur.nl>
2020-09-19 15:04:18 +0200

    software update

--- train_broccoli_amodal.py
+++ train_broccoli_amodal.py
@@ -22,9 +22,10 @@ if __name__ == "__main__":
 
     if torch.cuda.is_available():
         # register the amodal datasets 
-        register_coco_instances("broccoli_amodal_train", {}, "datasets/broccoli_amodal/train/annotations.json", "datasets/broccoli_amodal/train")
-        register_coco_instances("broccoli_amodal_val", {}, "datasets/broccoli_amodal/val/annotations.json", "datasets/broccoli_amodal/val")
-        register_coco_instances("broccoli_amodal_test", {}, "datasets/broccoli_amodal/test/annotations.json", "datasets/broccoli_amodal/test")
+        from detectron2.data.datasets import register_coco_instances
+        register_coco_instances("broccoli_amodal_train", {}, "datasets/20201231_size_experiment_realsense/train/annotations.json", "datasets/20201231_size_experiment_realsense/train")
+        register_coco_instances("broccoli_amodal_val", {}, "datasets/20201231_size_experiment_realsense/val/annotations.json", "datasets/20201231_size_experiment_realsense/val")
+        register_coco_instances("broccoli_amodal_test", {}, "datasets/20201231_size_experiment_realsense/test/annotations.json", "datasets/20201231_size_experiment_realsense/test")
 
         # create the metadata files 
         broccoli_amodal_train_metadata = MetadataCatalog.get("broccoli_amodal_train")
@@ -41,19 +42,18 @@ if __name__ == "__main__":
         cfg = get_cfg()
         cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_orcnn_X_101_32x8d_FPN_3x.yaml"))
         cfg.DATASETS.TRAIN = ("broccoli_amodal_train",)
-        cfg.DATASETS.VAL = ("broccoli_amodal_val",)
-        cfg.DATASETS.TEST = ("broccoli_amodal_test",)
+        cfg.DATASETS.TEST = ("broccoli_amodal_val",)
         cfg.NUM_GPUS = 2
         cfg.DATALOADER.NUM_WORKERS = 2
-        cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_X_101_32x8d_FPN_3x.yaml")
-        cfg.SOLVER.IMS_PER_BATCH = 1
+        cfg.MODEL.WEIGHTS = os.path.join("weights/broccoli_amodal_visible", "model_final.pth")
+        cfg.SOLVER.IMS_PER_BATCH = 2
         cfg.SOLVER.WEIGHT_DECAY = 0.0001
         cfg.SOLVER.LR_POLICY = 'steps_with_decay'
         cfg.SOLVER.BASE_LR = 0.02
         cfg.SOLVER.GAMMA = 0.1
         cfg.SOLVER.WARMUP_ITERS = 1000
-        cfg.SOLVER.MAX_ITER = 5000
-        cfg.SOLVER.STEPS = (0, 4000, 4500)
+        cfg.SOLVER.MAX_ITER = 10000
+        cfg.SOLVER.STEPS = (5000, 9000, 9500)
         cfg.SOLVER.CHECKPOINT_PERIOD = 1000
         cfg.MODEL.ROI_HEADS.NUM_CLASSES = 1  # only has one class (broccoli)
         cfg.OUTPUT_DIR = "weights/broccoli_amodal_temp"
